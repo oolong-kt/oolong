676c7a4184a6f2d6d3bf3045e327b252f1e93ac7
Michael Pardo <michael@michaelpardo.com>
2018-08-01 15:51:21 -0400

    Use only one dispatch function.

--- oolong/src/main/kotlin/oolong/runtime.kt
+++ oolong/src/main/kotlin/oolong/runtime.kt
@@ -5,26 +5,18 @@ fun <Model, Msg> runtime(program: Program<Model, Msg>): () -> Unit {
     val update = program.update
     val view = program.view
     val subscriptions = program.subscriptions
-    var state: Model
     var running = true
     fun reduce(update: Update<Model, Msg>) {
         if (!running) return
-        state = update.model
         val dispatch = { msg: Msg ->
             if (Thread.currentThread() != mainThread) {
-                throw IllegalThreadStateException("Dispatching must be done on the main thread.")
+                throw IllegalThreadStateException("Dispatch function must be called from the main thread.")
             }
-            reduce(update(msg, state))
-        }
-        val effDispatch = { msg: Msg ->
-            if (Thread.currentThread() == mainThread) {
-                throw IllegalThreadStateException("Cannot run side effects on the main thread.")
-            }
-            reduce(update(msg, state))
+            reduce(update(msg, update.model))
         }
         update.cmd(dispatch)
-        subscriptions(state)(effDispatch)
-        view(state, dispatch)
+        subscriptions(update.model)(dispatch)
+        view(update.model, dispatch)
     }
     reduce(program.init())
     return { running = false }
