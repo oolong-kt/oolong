0d76e258b0e9a3b67690c1a31083493dab4ea9e1
Michael Pardo <michael@michaelpardo.com>
2018-08-01 15:51:35 -0400

    Test that dispatch is called on the originating thread.

--- oolong/src/test/kotlin/oolong/RuntimeTest.kt
+++ oolong/src/test/kotlin/oolong/RuntimeTest.kt
@@ -5,12 +5,26 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import kotlin.concurrent.thread
 
 class RuntimeTest : Spek({
 
     given("a Counter runtime") {
         val program = Program(Counter.init, Counter.update, Counter.view)
         val terminate = runtime(program)
+
+        on("dispatch called off the main thread") {
+            it("throws an exception") {
+                thread {
+                    try {
+                        Counter.dispatch(Counter.Msg.Increment)
+                        assert(false)
+                    } catch (e: Exception) {
+                        assert(true)
+                    }
+                }.join()
+            }
+        }
 
         on("an increment message") {
             val count = Counter.model.count
