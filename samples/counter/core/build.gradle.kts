a699226c22b77b24e960bab0e17974c5a1a461fa
Michael Pardo <michael.pardo@instacart.com>
2019-02-20 09:40:10 -0500

    Make samples multiplatform.

--- samples/counter/core/build.gradle.kts
+++ samples/counter/core/build.gradle.kts
@@ -1,14 +1,39 @@
 import oolong.Dependencies
 
 plugins {
-    kotlin("jvm")
+    kotlin("multiplatform")
 }
 
 repositories {
     jcenter()
 }
 
-dependencies {
-    implementation(project(":oolong"))
-    implementation(Dependencies.Kotlin.StdLib)
+kotlin {
+    js()
+    jvm()
+    iosX64("ios")
+    linuxX64("linux")
+    macosX64("macOS")
+    mingwX64("windows")
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":oolong"))
+                implementation(Dependencies.Kotlin.StdLib.Common)
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(Dependencies.Kotlin.StdLib.JS)
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(Dependencies.Kotlin.StdLib.JDK8)
+            }
+        }
+    }
 }
