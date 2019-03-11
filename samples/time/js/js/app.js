dfb8b9b61a8855e691229146326a929c420132e9
Michael Pardo <michael.pardo@instacart.com>
2019-03-11 18:04:26 -0400

    Use pure js projects for js samples.

---
+++ samples/time/js/js/app.js
@@ -0,0 +1,26 @@
+(function (window) {
+	'use strict';
+
+	var Oolong = oolong.oolong.Oolong;
+	var Time = core.time.Time;
+	var h = superfine.h;
+	var patch = superfine.patch;
+
+	var node;
+
+	function render(props, dispatch) {
+		return h('div', {}, [
+			h('p', {}, new Date().toTimeString())
+		]);
+	}
+
+	Oolong.runtime(
+		Time.init,
+		Time.update,
+		Time.view,
+		function (props, dispatch) {
+			node = patch(node, render(props, dispatch), document.body);
+		}
+	)
+
+})(window);
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,16 +5,13 @@ include(":oolong")
 include(":samples:counter:core")
 include(":samples:counter:android")
 include(":samples:counter:javafx")
-include(":samples:counter:js")
 
 include(":samples:random:core")
 include(":samples:random:android")
 include(":samples:random:javafx")
-include(":samples:random:js")
 
 include(":samples:time:core")
 include(":samples:time:android")
 include(":samples:time:javafx")
-include(":samples:time:js")
 
 include(":samples:todo:core")
