cf5e549bc367ac0852d12c079a75a9c2675df4d3
Android Customers CircleCI <android-customers@instacart.com>
2019-03-29 12:00:26 -0700

    Add build scripts for js samples.

---
+++ samples/counter/js/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$BASH_SOURCE")
+
+echo "=============================="
+echo "Building Kotlin project..."
+echo "=============================="
+
+cd $BASEDIR/../../../
+./gradlew :samples:counter:core:build
+
+echo ""
+echo "=============================="
+echo "Installing NPM dependencies..."
+echo "=============================="
+echo ""
+
+cd $BASEDIR
+npm install
+
+echo "=============================="
+echo "Finished!"
+echo ""
+echo "$(pwd)/index.html"
+echo "=============================="
