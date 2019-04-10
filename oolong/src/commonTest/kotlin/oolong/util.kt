3b55d0613d65e1b4212650966c56d272c1311097
Michael Pardo <kensuke155@gmail.com>
2019-04-10 13:42:46 -0400

    Map fix (#40)
    * Fix effect map test.
    
    * Fix effect map dispatch.
    
    * Allow longer timeout for stack overflow test.

--- oolong/src/commonTest/kotlin/oolong/util.kt
+++ oolong/src/commonTest/kotlin/oolong/util.kt
@@ -2,13 +2,19 @@ package oolong
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
+import kotlin.test.fail
 
-fun runTest(block: suspend (resolve: () -> Unit) -> Unit) {
+fun runTest(timeout: Long = 1000, step: Long = 10, block: suspend (resolve: () -> Unit) -> Unit) {
     wrapBlocking {
+        var elapsed = 0L
         var resolved = false
         block { resolved = true }
         while (!resolved) {
-            delay(10)
+            delay(step)
+            elapsed += step
+            if (elapsed >= timeout) {
+                fail("Test exceeded max execution time of $timeout ms.")
+            }
         }
     }
 }
