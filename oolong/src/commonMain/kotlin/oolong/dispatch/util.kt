1b4e34f720f49f785d7cb5db620a19f9c3573d56
Michael Pardo <kensuke155@gmail.com>
2019-04-16 11:30:10 -0400

    Add Dispatch contramap. (#45)

---
+++ oolong/src/commonMain/kotlin/oolong/dispatch/util.kt
@@ -0,0 +1,9 @@
+package oolong.dispatch
+
+import oolong.Dispatch
+
+/**
+ * Contramap from [Dispatch] of [A] to [Dispatch] of [B]
+ */
+private fun <A, B> contramap(dispatch: Dispatch<A>, f: (B) -> A): Dispatch<B> =
+    { a -> dispatch(f(a)) }
