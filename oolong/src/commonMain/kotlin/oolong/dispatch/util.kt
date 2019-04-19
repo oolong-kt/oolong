a23ae016efaa36e3920dceb5640dff0e154a3707
Michael Pardo <michael@michaelpardo.com>
2019-04-19 14:10:07 -0400

    Fix arg name.

--- oolong/src/commonMain/kotlin/oolong/dispatch/util.kt
+++ oolong/src/commonMain/kotlin/oolong/dispatch/util.kt
@@ -6,4 +6,4 @@ import oolong.Dispatch
  * Contramap from [Dispatch] of [A] to [Dispatch] of [B]
  */
 fun <A, B> contramap(dispatch: Dispatch<A>, f: (B) -> A): Dispatch<B> =
-    { a -> dispatch(f(a)) }
+    { b -> dispatch(f(b)) }
