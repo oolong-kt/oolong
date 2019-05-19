9f0e4ca3c0ce43c3ace7aae3a2c207204b5a7afe
Michael Pardo <michael@michaelpardo.com>
2019-05-19 10:01:37 -0400

    Add usage docs.

--- oolong/src/commonMain/kotlin/oolong/dispatch/util.kt
+++ oolong/src/commonMain/kotlin/oolong/dispatch/util.kt
@@ -4,6 +4,34 @@ import oolong.Dispatch
 
 /**
  * Contramap from [Dispatch] of [A] to [Dispatch] of [B]
+ *
+ * `contramap` can be used to map a dispatch function from a parent `Msg` to a child `Msg`. A typical use for
+ * `contramap` is compositional screen management.
+ *
+ * Example:
+ *
+ * ```kotlin
+ * sealed class Props {
+ *     data class Home(val props: HomeProps): Props()
+ *     data class Settings(val props: SettingsProps): Props()
+ * }
+ *
+ * sealed class Msg {
+ *     data class Home(val msg: HomeMsg) : Msg()
+ *     data class Settings(val msg: SettingsMsg) : Msg()
+ * }
+ *
+ * val render: Render<Msg, Props> = { props: Props, dispatch: Dispatch<Msg> ->
+ *     when (props) {
+ *         is Props.Home -> {
+ *             homeRender(props.props, contramap(dispatch, Msg::Home))
+ *         }
+ *         is Props.Settings -> {
+ *             settingsRender(props.props, contramap(dispatch, Msg::Settings))
+ *         }
+ *     }
+ * }
+ * ```
  */
 fun <A : Any, B : Any> contramap(dispatch: Dispatch<A>, f: (B) -> A): Dispatch<B> =
     { b -> dispatch(f(b)) }
--- oolong/src/commonMain/kotlin/oolong/next/util.kt
+++ oolong/src/commonMain/kotlin/oolong/next/util.kt
@@ -5,6 +5,47 @@ import oolong.effect.map
 
 /**
  * Map from [Next] of [A] and [B] to [Next] of [C] and [D]
+ *
+ * `bimap` can be used to map an init or update function from a child `Next` to a parent `Next`. A typical use for
+ * `bimap` is compositional screen management.
+ *
+ * Example:
+ *
+ * ```kotlin
+ * sealed class Model {
+ *     data class Home(val model: HomeModel): Model()
+ *     data class Settings(val model: SettingsModel): Model()
+ * }
+ *
+ * sealed class Msg {
+ *     data class Home(val msg: HomeMsg) : Msg()
+ *     data class Settings(val msg: SettingsMsg) : Msg()
+ * }
+ *
+ * val update: Update<Model, Msg> = { msg: Msg, model: Model ->
+ *     when (msg) {
+ *         is Msg.Home -> {
+ *             when (model) {
+ *                 is Model.Home -> bimap(
+ *                     homeUpdate(msg.msg, model.model),
+ *                     Model::Home,
+ *                     Msg::Home
+ *                 )
+ *                 else -> model to none()
+ *             }
+ *         }
+ *         is Msg.Settings -> {
+ *             when (model) {
+ *                 is Model.Settings -> bimap(
+ *                     settingsUpdate(msg.msg, model.model),
+ *                     Model::Settings,
+ *                     Msg::Settings
+ *                 )
+ *                 else -> model to none()
+ *             }
+ *         }
+ *     }
+ * }
  */
 fun <A : Any, B : Any, C : Any, D : Any> bimap(next: Next<A, B>, fa: (A) -> C, fb: (B) -> D): Next<C, D> =
     fa(next.first) to map(next.second, fb)
