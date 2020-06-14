2509cdae2462c54c19cb8353ce9eecb1b4860f6c
Michael Pardo <michael@michaelpardo.com>
2020-06-14 15:13:24 -0400

    Add router direction. Fix restoration logic.

--- oolong-extensions/oolong-navigation/src/commonMain/kotlin/oolong/navigation/Router.kt
+++ oolong-extensions/oolong-navigation/src/commonMain/kotlin/oolong/navigation/Router.kt
@@ -18,16 +18,20 @@ abstract class Router<Model_ : Any, Msg_ : Any, Props : Any, Route : Any> {
     sealed class Msg<out Msg_, out Route> {
 
         data class SetRoute<Route : Any>(
-            val route: Route,
-            val lastRouteKey: String,
+            val nextRoute: Route,
+            val prevRouteKey: String,
             val nextRouteKey: String,
-            val restore: Boolean
+            val direction: Direction
         ) : Msg<Nothing, Route>()
 
         data class Route<Msg_>(
             val screenMsg: Msg_
         ) : Msg<Msg_, Nothing>()
 
+    }
+
+    enum class Direction {
+        FORWARD, BACKWARD, REPLACE
     }
 
     // Init
@@ -59,17 +63,19 @@ abstract class Router<Model_ : Any, Msg_ : Any, Props : Any, Route : Any> {
 
     private val updateSetRoute: (Msg.SetRoute<Route>, Model<Model_>) -> Next<Model<Model_>, Msg<Msg_, Route>> =
         { msg, model ->
-            val savedModel: Model_? = if (msg.restore) model.routeCache[msg.lastRouteKey] else null
-            if (savedModel != null) {
+            val savedModel: Model_? = model.routeCache[msg.nextRouteKey]
+            if (msg.direction == Direction.BACKWARD && savedModel != null) {
                 model.copy(
                     routeModel = savedModel,
-                    routeCache = model.routeCache - msg.lastRouteKey
+                    routeCache = model.routeCache - msg.nextRouteKey
                 ) to none()
             } else {
-                val (delegate, effect) = initRoute(msg.route)()
+                val (delegate, effect) = initRoute(msg.nextRoute)()
+                val routeCache = if (msg.direction == Direction.REPLACE) emptyMap()
+                else model.routeCache + (msg.prevRouteKey to model.routeModel)
                 model.copy(
                     routeModel = delegate,
-                    routeCache = model.routeCache + (msg.nextRouteKey to model.routeModel)
+                    routeCache = routeCache
                 ) to map(effect, { screenMsg -> Msg.Route(screenMsg) })
             }
         }
