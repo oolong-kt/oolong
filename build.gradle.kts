bdee34007e59c4b034f7c47d1e9ba668aee4b6e9
Michael Pardo <michael@michaelpardo.com>
2022-07-28 19:51:08 -0400

    Borrow some CI scripts from Okio.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,57 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
-    alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.maven.publish)
 }
 
+allprojects {
+    group = project.property("GROUP") as String
+    version = project.property("VERSION_NAME") as String
+
+    repositories { mavenCentral() }
+
+    tasks.withType<DokkaTask>().configureEach {
+        dokkaSourceSets.configureEach {
+            reportUndocumented.set(false)
+            skipDeprecated.set(true)
+            jdkVersion.set(8)
+        }
+
+        if (name == "dokkaHtml") {
+            outputDirectory.set(file("${rootDir}/docs"))
+        }
+    }
+
+    plugins.withId("com.vanniktech.maven.publish.base") {
+        configure<MavenPublishBaseExtension> {
+            publishToMavenCentral(SonatypeHost.S01)
+            signAllPublications()
+            pom {
+                description.set("MVU for Kotlin Multiplatform")
+                name.set(project.name)
+                url.set("https://github.com/oolong-kt/oolong/")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/oolong-kt/oolong/")
+                    connection.set("scm:git:git://github.com/oolong-kt/oolong.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/oolong-kt/oolong.git")
+                }
+                developers {
+                    developer {
+                        id.set("oolong")
+                        name.set("Oolong")
+                    }
+                }
+            }
+        }
+    }
+}
