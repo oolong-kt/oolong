23e0520aac4fb16c6f0203bdcee7732588f72ca6
Michael Pardo <michael@michaelpardo.com>
2022-07-30 15:17:57 -0400

    Borrow some CI scripts from Okio. (#138)

--- oolong/build.gradle.kts
+++ oolong/build.gradle.kts
@@ -1,28 +1,21 @@
+import com.vanniktech.maven.publish.JavadocJar.Dokka
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinNativeTargetPreset
 
-val GROUP: String by project
-val VERSION_NAME: String by project
-
-group = GROUP
-version = VERSION_NAME
-
 plugins {
-    kotlin("multiplatform")
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.maven.publish)
 }
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 kotlin {
     jvm()
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                api(libs.kotlin.coroutines.core)
-            }
-        }
+        val commonMain by getting { dependencies { api(libs.kotlin.coroutines.core) } }
 
         val jvmTest by getting {
             dependencies {
@@ -37,24 +30,25 @@ kotlin {
     targets {
         // Print supported targets with:
         // scripts/fetch_native_targets org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4
-        val supportedTargets = setOf(
-            "ios_arm32",
-            "ios_arm64",
-            "ios_simulator_arm64",
-            "ios_x64",
-            "linux_x64",
-            "macos_arm64",
-            "macos_x64",
-            "mingw_x64",
-            "tvos_arm64",
-            "tvos_simulator_arm64",
-            "tvos_x64",
-            "watchos_arm32",
-            "watchos_arm64",
-            "watchos_simulator_arm64",
-            "watchos_x64",
-            "watchos_x86",
-        )
+        val supportedTargets =
+            setOf(
+                "ios_arm32",
+                "ios_arm64",
+                "ios_simulator_arm64",
+                "ios_x64",
+                "linux_x64",
+                "macos_arm64",
+                "macos_x64",
+                "mingw_x64",
+                "tvos_arm64",
+                "tvos_simulator_arm64",
+                "tvos_x64",
+                "watchos_arm32",
+                "watchos_arm64",
+                "watchos_simulator_arm64",
+                "watchos_x64",
+                "watchos_x86",
+            )
 
         presets
             .filterIsInstance<AbstractKotlinNativeTargetPreset<*>>()
@@ -71,4 +65,6 @@ kotlin {
     }
 }
 
-// apply("$rootDir/gradle/gradle-mvn-mpp-push.gradle")
+configure<MavenPublishBaseExtension> {
+    configure(KotlinMultiplatform(javadocJar = Dokka("dokkaGfm")))
+}
