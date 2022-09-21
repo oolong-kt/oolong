24eb5f6062e238ab1eb8ce397e17941366b96a9c
Michael Pardo <mpardo@netflix.com>
2022-09-21 10:38:51 -0400

    Apply Spotless plugin with ktfmt.

--- oolong/src/commonMain/kotlin/oolong/effect/util.kt
+++ oolong/src/commonMain/kotlin/oolong/effect/util.kt
@@ -3,25 +3,18 @@ package oolong.effect
 import kotlinx.coroutines.launch
 import oolong.Effect
 
-/**
- * Create an empty [Effect].
- */
+/** Create an empty [Effect]. */
 fun <Msg> none(): Effect<Msg> = {}
 
-/**
- * Compose [effects] into a single [Effect].
- */
-fun <Msg> batch(vararg effects: Effect<Msg>): Effect<Msg> =
-    batch(effects.asIterable())
+/** Compose [effects] into a single [Effect]. */
+fun <Msg> batch(vararg effects: Effect<Msg>): Effect<Msg> = batch(effects.asIterable())
 
-/**
- * Compose [effects] into a single [Effect].
- */
-fun <Msg> batch(effects: Iterable<Effect<Msg>>): Effect<Msg> =
-    { dispatch -> for (effect in effects) launch { effect(dispatch) } }
+/** Compose [effects] into a single [Effect]. */
+fun <Msg> batch(effects: Iterable<Effect<Msg>>): Effect<Msg> = { dispatch ->
+    for (effect in effects) launch { effect(dispatch) }
+}
 
-/**
- * Map [effect] of type [A] to [Effect] of [B] using [f].
- */
-fun <A, B> map(effect: Effect<A>, f: (A) -> B): Effect<B> =
-    { dispatch -> effect { a -> dispatch(f(a)) } }
+/** Map [effect] of type [A] to [Effect] of [B] using [f]. */
+fun <A, B> map(effect: Effect<A>, f: (A) -> B): Effect<B> = { dispatch ->
+    effect { a -> dispatch(f(a)) }
+}
